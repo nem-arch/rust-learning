3dd47edea203cf6aee65ee70a78be466595e7504
nem <nem@posteo.net>
2014-09-20 05:31:25 +0200

    improve generation of data

--- net/src/main.rs
+++ net/src/main.rs
@@ -1,34 +1,36 @@
 use std::io;
 use std::rand::{task_rng, Rng};
-use std::num;
 use std::io::timer;
 use std::time::Duration;
 
-fn gen_dots(x: int, y: int) -> (int, int) {
-    let mut rng = task_rng();
-    return ( num::div_rem(rng.gen(),255i).val1(), num::div_rem(rng.gen(),255i).val1() )
-}
 
-fn handle_write(mut stream: io::TcpStream) -> io::IoResult<()> {
-    let (x,y) = gen_dots(1,1);
-    let (u,v) = gen_dots(x,y);
-    let s = format!("PX {} {} {}{}00",x,y,format!("{:X}",u),format!("{:X}",v));
-	stream.write_line(s.as_slice())
+fn r_n (n: uint) -> Vec<u8> {
+    // generate vector of random u8
+    task_rng()
+        .gen_iter::<u8>()
+        .take(n)
+        .collect()
 }
 
 fn main() {
 	let host = "192.168.0.23";
 	let port = 1234;
-	let stream = match io::TcpStream::connect(host, port) {
+	let mut stream = match io::TcpStream::connect(host, port) {
         Ok(s) => s,
         Err(e) => fail!("{}",e)
     };
-	loop {
-		match handle_write(stream.clone()) {
-			Err(e) => println!("Write Error: {}", e),
-			//Ok(()) => handle_readback(stream.clone())
-            _ => { timer::sleep(Duration::milliseconds(5))}
-		}
-	}
+    loop {
+        let p: Vec<u8> = r_n(2u);
+        let c: Vec<String> = r_n(3u)
+            .iter()
+            .map(|&x| format!("{:X}",x) )
+            .collect();
+        let s = format!("PX {} {} {}{}{}",p[0],p[1],c[0],c[1],c[2]);
+        match stream.write_line(s.as_slice()) {
+            Err(e) => fail!("Write Error: {}", e),
+            Ok(()) => { }
+        }
+        timer::sleep(Duration::milliseconds(5));
+    }
 }
 
