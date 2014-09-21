8799e760d1dffe4f1c05b8f1d855f40b0a9e8d86
nem <nem@posteo.net>
2014-09-21 21:49:44 +0200

    try handling quit better

--- irc/src/main.rs
+++ irc/src/main.rs
@@ -10,42 +10,53 @@ fn start(argc: int, argv: *const *const u8) -> int {
         green::start(argc, argv, rustuv::event_loop, main)
 }
 fn irc(rx: Receiver<Result<(),()>>) {
-    let nick = "rust_test";
+    let nick = "rust_test_";
     let host = "irc.freenode.net";
     let port = 6667;
-    let mut buff = [0u8, .. 1024];
+    let mut buff = [0u8, .. 128];
     let mut sock = match TcpStream::connect(host, port) {
         Ok(s) => s,
         Err(e) => fail!("{}",e)
     };
     sock.write_line(format!("NICK {}",nick).as_slice()).unwrap();
     sock.write_line(format!("USER {} {} {}: {}",nick,nick,nick,nick).as_slice()).unwrap();
+    println!("SENT LOGIN");
+
     loop {
         match sock.read(buff) {
-            Ok(uint) => { println!("{}",str::from_utf8(buff)) },
-            Err(e) => println!("ERROR: {}",e)
+            Ok(_) => { println!("{}",str::from_utf8(buff).unwrap()) },
+            Err(e) => println!("ERROR READING BUFFER: {}",e)
         };
         match rx.try_recv() {
-            Err(Empty) => { },
-            Ok(_) => { sock.write_line("QUIT"); break; }
+            Ok(_) => {
+                match sock.write_line("QUIT") {
+                    Err(e) => println!("ERROR SENDING QUIT: {}",e),
+                    Ok(()) => {
+                        println!("QUIT SENT");
+                        break;
+                    }
+                };
+            },
+            _ => ()
         }
     }
-    println!("QUIT");
+    sock.read(buff).unwrap();
+    println!("{}",str::from_utf8(buff).unwrap());
 }
 
 fn main() {
     let mut sighandler = signal::Listener::new();
     match sighandler.register(Interrupt) {
-        Ok(()) => { },
-        Err(e) => { fail!(format!("{}",e)) }
+        Err(e) => { fail!(format!("{}",e)) },
+        _ => ()
     };
     match sighandler.register(Break) {
-        Ok(()) => { },
-        Err(e) => { fail!(format!("{}",e)) }
+        Err(e) => { fail!(format!("{}",e)) },
+        _ => ()
     };
     match sighandler.register(Quit) {
-        Ok(()) => { },
-        Err(e) => { fail!(format!("{}",e)) }
+        Err(e) => { fail!(format!("{}",e)) },
+        _ => ()
     };
     let (tx, rx) = channel::<Result<(),()>>();
     spawn(proc() {
@@ -53,8 +64,9 @@ fn main() {
     });
     loop {
         match sighandler.rx.recv() {
-            Interrupt|Break|Quit => tx.send(Ok(())),
+            Interrupt|Break|Quit => { tx.send(Err(())); break; },
             _ => ()
         }
     }
+    println!("END");
 }
