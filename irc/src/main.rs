faecc696c2e9f535682fb318504576b9f4a05063
nem <nem@posteo.net>
2014-09-22 05:08:26 +0200

    check readerrortypes, react accordingly read still hogging cpu

--- irc/src/main.rs
+++ irc/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::TcpStream;
 use std::io;
 use std::str;
+use std::io::timer;
+use std::time::Duration;
 
 fn main() {
     let nick = "rust_test_";
@@ -8,7 +10,7 @@ fn main() {
     let port = 6667;
     let mut sock = match TcpStream::connect(host, port) {
         Ok(s) => s,
-        Err(e) => fail!("{}",e)
+        Err(e) => fail!("SOCKERROR: {}",e)
     };
     sock.write_line(format!("NICK {}",nick).as_slice()).unwrap();
     sock.write_line(format!("USER {} {} {}: {}",nick,nick,nick,nick).as_slice()).unwrap();
@@ -16,16 +18,25 @@ fn main() {
     let (cmdtx, cmdrx) = channel::<String>();
     let mut sock_write = sock.clone();
     let mut sock_read = sock.clone();
+    let mut buff = [0u8, .. 4096];
     spawn(proc() {
         loop {
+            sock_read.set_read_timeout(Some(100));
             // hogs cpu
-            match sock_read.read_byte() {
-                Ok(s) => {
-                    print!("{}",str::from_utf8(&[s]).unwrap());
+            match sock_read.read(buff) {
+                Ok(i) => {
+                    if i<buff.len() {
+                        buff[i] = 0;
+                    }
+                    print!("{}",str::from_utf8(buff).unwrap());
                 },
-                Err(e) => {
-                    println!("ERROR READING BUFFER: {}",e);
-                    break;
+                Err(ref e) => {
+                    //println!("ERROR READING BUFFER: {}",e.desc);
+                    match e.kind {
+                        io::TimedOut => timer::sleep(Duration::seconds(1)),
+                        io::EndOfFile => break,
+                        _ => ()
+                    }
                 }
             };
         }
