d9179ba961d123ae0db06ffb5df1de58809c262b
nem <nem@posteo.net>
2014-09-23 01:45:35 +0200

    complete rewrite, create irc mod

--- irc/src/main.rs
+++ irc/src/main.rs
@@ -1,73 +1,126 @@
-use std::io::TcpStream;
 use std::io;
-use std::str;
-use std::io::timer;
-use std::time::Duration;
+use irc::{Net, User};
 
-fn main() {
-    let nick = "rust_test_";
-    let host = "irc.freenode.net";
-    let port = 6667;
-    let mut sock = match TcpStream::connect(host, port) {
-        Ok(s) => s,
-        Err(e) => fail!("SOCKERROR: {}",e)
-    };
-    sock.write_line(format!("NICK {}",nick).as_slice()).unwrap();
-    sock.write_line(format!("USER {} {} {}: {}",nick,nick,nick,nick).as_slice()).unwrap();
-    println!("SENT LOGIN");
-    let (cmdtx, cmdrx) = channel::<Result<String, ()>>();
-    let mut sock_write = sock.clone();
-    let mut sock_read = sock.clone();
-    let mut buff = [0u8, .. 512];
-    spawn(proc() {
-        loop {
-            sock_read.set_read_timeout(Some(10));
-            match sock_read.read(buff) {
-                Ok(i) => {
-                    if i<buff.len() {
-                        for j in range(i, buff.len()) {
-                            buff[j] = 0;
+mod irc {
+    use std::io;
+    use std::io::TcpStream;
+    use std::io::timer;
+    use std::time::Duration;
+    use std::str;
+    #[deriving(Clone)]
+    pub struct Net {
+        pub host: String,
+        pub port: u16,
+    }
+    #[deriving(Clone)]
+    pub struct User {
+        pub nick: String,
+    }
+    struct Sock {
+        pub cmd_rx: Receiver<Result<String, ()>>,
+        pub net: TcpStream
+    }
+    impl Net {
+        pub fn output(&mut self, net: &mut TcpStream) {
+            let mut buff = [0u8, .. 512];
+            loop {
+                net.set_read_timeout(Some(10));
+                match net.read(buff) {
+                    Ok(i) => {
+                        if i<buff.len() {
+                            for j in range(i, buff.len()) {
+                                buff[j] = 0;
+                            }
+                        }
+                        print!("{}",str::from_utf8(buff).unwrap());
+                    },
+                    Err(ref e) => {
+                        //println!("ERROR READING BUFFER: {}",e.desc);
+                        match e.kind {
+                            io::TimedOut => timer::sleep(Duration::seconds(1)),
+                            io::EndOfFile => break,
+                            _ => ()
                         }
                     }
-                    print!("{}",str::from_utf8(buff).unwrap());
-                },
-                Err(ref e) => {
-                    //println!("ERROR READING BUFFER: {}",e.desc);
-                    match e.kind {
-                        io::TimedOut => timer::sleep(Duration::seconds(1)),
-                        io::EndOfFile => break,
-                        _ => ()
-                    }
-                }
-            };
-        }
-    });
-    spawn(proc() {
-        loop {
-            match cmdrx.recv() {
-                Ok(s) => {
-                    println!("COMMAND {}",s);
-                    if s.as_slice() == "quit\n" {
-                        sock_write.write_line("QUIT").unwrap();
-                        break;
-                    }
-                },
-                _ => ()
+                };
             }
         }
-    });
+    }
+    impl User {
+        pub fn connect(&mut self, net: &mut Net) -> Result<Sender<Result<String, ()>>, String> {
+            let (tx, rx) = channel::<Result<String,()>>();
+            let mut sock = Sock {
+                net: match TcpStream::connect(net.host.as_slice(), net.port) {
+                    Ok(stream) => stream,
+                    Err(e) => return Err(format!("{}",e).to_string()),
+                },
+                cmd_rx: rx,
+            };
+            let mut n = sock.net.clone();
+            self.apply(&mut sock);
+            let mut user_in = self.clone();
+            spawn(proc() {
+                user_in.input(&mut sock);
+            });
+            let mut net_out = net.clone();
+            spawn(proc() {
+                net_out.output(&mut n);
+            });
+            Ok(tx)
+        }
+        pub fn apply(&mut self, sock: &mut Sock) {
+            sock.net.write_line(format!("NICK {}",
+                                        self.nick
+                                       ).as_slice()
+                               ).unwrap();
+            sock.net.write_line(format!("USER {} {} {}: {}",
+                                        self.nick,
+                                        self.nick,
+                                        self.nick,
+                                        self.nick
+                                       ).as_slice()
+                                ).unwrap();
+        }
+        pub fn input(&mut self, sock: &mut Sock) {
+            loop {
+                match sock.cmd_rx.recv() {
+                    Ok(s) => {
+                        println!("COMMAND {}",s);
+                        if s.as_slice() == "/quit\n" {
+                            sock.net.write_line("QUIT").unwrap();
+                            break;
+                        }
+                    },
+                    _ => ()
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut irc_net = irc::Net {
+        host: "irc.freenode.net".to_string(),
+        port: 6667,
+    };
+    let mut irc_user = irc::User {
+        nick: "rust_test".to_string(),
+    };
+    let tx: Sender<Result<String, ()>> = match irc_user.connect(&mut irc_net) {
+        Ok(s) => s,
+        Err(e) => fail!(e),
+    };
     let mut cmd: String = "none".to_string();
     loop {
         match io::stdin().read_line() {
             Ok(s) => {
                 cmd = s.clone();
-                cmdtx.send(Ok(s));
+                tx.send(Ok(s));
             },
             _ => {}
         };
-        if cmd.as_slice() == "quit\n" {
+        if cmd.as_slice() == "/quit\n" {
             break;
         }
-}
-println!("END");
+    }
 }
