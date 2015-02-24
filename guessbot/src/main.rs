73f9db1e16c6e9e15ae19a2f8f0b8e64b7956a4d
nem <nem@posteo.net>
2015-02-24 15:22:16 +0100

    botprogram playing guess with itself

---
+++ guessbot/src/main.rs
@@ -0,0 +1,32 @@
+use std::rand;
+use std::cmp::Ordering;
+
+fn main() {
+    let mut min = 0;
+    let mut max = 99;
+    let mut cur = 50;
+    let secret = rand::random::<u32>()%100u32;
+    loop {
+        println!("{} ?= {}", cur, secret);
+        match cmp(cur, secret) {
+            Ordering::Less => {
+                min = cur;
+                cur += (max - min)/2;
+            }
+            Ordering::Greater => {
+                max = cur;
+                cur -= (max - min)/2;
+            }
+            Ordering::Equal => {
+                println!("i win!");
+                break;
+            }
+        }
+    }
+}
+
+fn cmp(a: u32, b: u32) -> Ordering {
+    if a < b { Ordering::Less }
+    else if a > b { Ordering::Greater }
+    else { Ordering::Equal }
+}
